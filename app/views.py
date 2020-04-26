e384a89049bc59d3f9a7a8ad9e1a61a76e6dbd99
mercy-shii <wemercy525@gmail.com>
2020-04-26 16:16:57 +0300

    changes

--- app/views.py
+++ app/views.py
@@ -8,8 +8,11 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    #Getting popular source
+    popular_sources = get_sources('popular')
+    print(popular_sources)
     title = 'News-articles'
-    return render_template('index.html',title = title)
+    return render_template('index.html',title = title,popular = popular_sources)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
