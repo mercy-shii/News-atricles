8344f8f0350b1b0627916d784c10d3e1d3cf58eb
mercy-shii <wemercy525@gmail.com>
2020-04-26 17:19:54 +0300

    changes

--- app/request.py
+++ app/request.py
@@ -1,6 +1,6 @@
 from app import app
 import urllib.request,json
-from .models import source
+from .models import Source
 
 Source = source.Source
 
@@ -10,24 +10,24 @@ api_key = app.config['NEWS_API_KEY']
 #Getting the source base url
 base_url = app.config["NEWS_API_BASE_URL"]
 
-def get_sources(category):
+def get_source(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_sources_url = base_url.format(category,api_key)
+    get_source_url = base_url.format(category,api_key)
 
-    with urllib.request.urlopen(get_sources_url) as url:
-        get_sources_data = url.read()
-        get_sources_response = json.loads(get_sources_data)
+    with urllib.request.urlopen(get_source_url) as url:
+        get_source_data = url.read()
+        get_source_response = json.loads(get_source_data)
 
-        sources = None
+        source_results = None
 
-        if get_sources_response['sources']:
-            source_sources_list = get_source_response['sources']
-            source_sources = process_sources(sources_source_list)
+        if get_source_response['sources']:
+            source_result_list = get_source_response['sources']
+            source_results = process_source(sources_result_list)
 
 
-    return source_sources
+    return source_results
 
 def process_sources(source_list):
     '''
