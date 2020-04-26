54d9277cd64254c664ddde12edc7f72f053d92f6
mercy-shii <wemercy525@gmail.com>
2020-04-26 17:36:04 +0300

    changes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_source
+from .request import get_sources
 
 #views
 @app.route('/')
@@ -10,7 +10,7 @@ def index():
     View root page function that returns the index page and its data
     '''
     #Getting popular source
-    general_sources = get_source('general')
+    general_sources = get_sources('general')
     print(general_sources)
     title = 'News-articles'
     return render_template('index.html',title = title,general = general_sources)
