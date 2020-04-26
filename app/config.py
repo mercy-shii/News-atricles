31bd37a0e40d6779f91390d9ba7f051881601fe2
mercy-shii <wemercy525@gmail.com>
2020-04-26 19:45:25 +0300

    changes

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SOURCES_API_BASE_URL = 'https://newsapi.org/v2/sources?&apiKey={}'
+    SOURCES_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
 
 
 
--- app/source_test.py
+++ app/source_test.py
@@ -12,7 +12,7 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Source('abc-news','ABC News','All country news','www.abcnews.com')
+        self.new_source = Source('abcnews','abc','News that keeps you up to date','https:abc.org','general','en','us')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
